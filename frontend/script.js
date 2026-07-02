56f8fd13879430a67e8dd7dc8b16c29f9fa1124f
GustavoR <iprossdemoura@gmail.com>
2026-07-02 20:51:34 -0300

    v2.4.2

--- frontend/script.js
+++ frontend/script.js
@@ -2,7 +2,7 @@ const API =
 window.location.hostname === "localhost" ||
 window.location.hostname === "127.0.0.1"
 ? "http://localhost:3000"
-: "https://algumacoisa.com"; // Substitua pelo seu domínio de produção
+: "https://criticalhit.onrender.com";
 
 
 //acontecer antes de tudo//
